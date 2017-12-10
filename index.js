ba83d5d9c5f71a891d21300413436a5b7dfeb662
Aurelien Perronneau <aurelien@monolithmtl.com>
2017-12-10 05:18:00 -0500

    HouseHolding

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var express   = require('express'),
     app       = express(),
-    expressWs = require('express-ws')(app),
-    hbs       = require('express-hbs');
+    ws        = require('express-ws')(app),
+    exec      = require('child_process').exec;
 
 // Use `.html` for extensions
 app.use(express.static(__dirname));
 
 // Optional middleware
 app.use(function (req, res, next) {
-  console.log('middleware');
+  console.log('Optional middleware');
   return next();
 });
 
@@ -19,14 +19,14 @@ app.get('/', function(req, res, next){
 
 // WebSocket
 app.ws('/', function(ws, req) {
-
   // Handler
   ws.on('message', function(msg) {
-    console.log(msg);
+    console.log('Received message : ' + msg);
   });
-  console.log('Connection opened !');
+
 });
 
 app.listen(3000, function(){
-  console.log("~ Listening on http://localhost:3000/ ");
+  console.log("Listening on http://localhost:3000 !");
+  exec('open http://localhost:3000/')
 });
